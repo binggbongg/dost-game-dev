694805deda74a2cabd8436f2fcd472d872e4104e
AJ <angelajahziel@gmail.com>
2026-06-24 11:01:04 +0800

    added temporary mana cost for testing

--- scenes/playground/mana_manager.gd
+++ scenes/playground/mana_manager.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 signal mana_changed(current_mana: int)
 
-var max_mana: int = 3 # Starting mana
-var current_mana: int = 0
+var max_mana: int = 100 # Starting mana
+var current_mana: int = 100
 
 func reset_turn_mana():
 	# Standard card game: max mana increases by 1 each turn (cap at 10)
@@ -13,8 +13,11 @@ func reset_turn_mana():
 
 func spend_mana(amount: int) -> bool:
 	if current_mana >= amount:
+		print("Current mana before " + str(current_mana))
 		current_mana -= amount
+		print("Current mana after " + str(current_mana))
 		mana_changed.emit(current_mana)
+	
 		return true
 	return false
 
--- scenes/playground/turn_manager.gd
+++ scenes/playground/turn_manager.gd
@@ -21,7 +21,7 @@ func end_player_turn():
 	change_state(GameEnums.TurnState.ENEMY_TURN)
 	print("ENEMY TURN: Player is locked out for 5 seconds.")
 	
-	await get_tree().create_timer(30.0).timeout 
+	await get_tree().create_timer(1.0).timeout 
 	
 	# After 30s, the state goes back to Player Action (no need to click draw again!)
 	start_player_turn()
