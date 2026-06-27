b348bb9cbcee90537dcc93ad44d724d414bcefb6
AJ <angelajahziel@gmail.com>
2026-06-27 14:16:44 +0800

    reusable coindisplay

--- script/ui/shopUI.gd
+++ script/ui/shopUI.gd
@@ -2,14 +2,13 @@ extends "res://script/ui/base_menu.gd"
 
 @export var slot_scene: PackedScene = preload("res://scenes/menus/shop_item_slot.tscn")
 @export var shop_data: ShopData 
+
 @onready var item_grid: GridContainer = $"HBoxContainer/Left Column/ItemGrid"
-@onready var coin_label = $CoinDisplay/Coins
 @onready var name_label = $HBoxContainer/DetailsPanel/NameLabel
 @onready var desc_label = $HBoxContainer/DetailsPanel/Description
 @onready var big_icon = $HBoxContainer/DetailsPanel/BigIcon
 @onready var purchase_button: TextureButton = $HBoxContainer/DetailsPanel/purchase_button
 @onready var cost: Label = $HBoxContainer/DetailsPanel/Cost
-
 
 var selected_item_id: String = ""
 var selected_price: int = 0
@@ -17,8 +16,6 @@ var selected_price: int = 0
 func _ready():
 	super._ready() 
 	purchase_button.pressed.connect(_on_purchase_clicked)
-	PlayerProfile.coins_changed.connect(update_label)
-	update_label(PlayerProfile.coins)
 	populate_shop()
 
 func populate_shop():
@@ -32,7 +29,6 @@ func populate_shop():
 			final_id = item.item_id 
 		else:
 			final_id = str(item)    
-			
 		
 		slot.set_item(final_id)
 		slot.item_selected.connect(_on_item_selected)
@@ -44,15 +40,14 @@ func _on_item_selected(id: String):
 	selected_item_id = id
 	selected_price = data.price
 	
-
 	name_label.text = data.get("item_name") if data.get("item_name") else data.get("name")
 	desc_label.text = data.description
 	cost.text = str(data.price)
 
 	var tex = data.get("icon") if data.get("icon") else data.get("texture")
 	big_icon.texture = tex
-
 	big_icon.custom_minimum_size = Vector2(250, 350)
+
 func _on_purchase_clicked():
 	if selected_item_id == "": return
 	if ShopManager.buy_item(selected_item_id, selected_price):
@@ -60,6 +55,3 @@ func _on_purchase_clicked():
 		
 	else:
 		print("UI: Purchase Failed!")
-
-func update_label(val):
-	coin_label.text =  str(val)
