00139df01e47ff1a886e9011bca1eb225af5836d
AJ <angelajahziel@gmail.com>
2026-07-02 23:32:34 +0800

    draft script fixing pa

--- script/story/storyUI.gd
+++ script/story/storyUI.gd
@@ -1,14 +1,16 @@
 extends CanvasLayer
 
 @onready var me: AnimatedSprite2D = $Buttons/Content/CharacterSpotlight/Character
+var player_name
 
 func _ready():
+	if PlayerProfile.player_name != "Default Player":
+		player_name = PlayerProfile.player_name
 	if PlayerProfile.selected_character != "None":
 		var character_id = PlayerProfile.selected_character
 		var path = "res://data/Characters/%s.tres" % character_id
-
-		var frames := load(path) as SpriteFrames
-		if frames:
-			me.sprite_frames = frames
-		else:
-			push_error("Failed to load SpriteFrames: " + path)
+		var character_data := load(path) as CharacterData
+		if character_data:
+			me.sprite_frames = character_data.sprite_frames
+			me.play("idle")
+			
